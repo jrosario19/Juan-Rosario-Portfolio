c69e5bb82e3820904b0555a76f58b821c1b1fc06
Juan Rosario <jrosario19@hotmail.com>
2022-05-06 19:58:33 -0400

    Update index

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ const listOfWork = [{
   Description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea.",
   popImg: 'images/Project1ScreenShot.jpg',
   techs: ['css', 'JavaScript', 'html', 'Codekit', 'GitHub', 'Bootstrap'],
-  liveLink: 'https://jrosario19.github.io/Capstone-Project-1/',
+  liveLink: 'https://jrosario19.github.io/6to.-Symposium-of-Industrial-Engineering/',
   sourceLink: 'https://github.com/jrosario19/Capstone-Project-1',
 }, {
   Id: '2',
